7a99c59efc7b339d8f8470904b62bb8251c1d32a
Knob <59198478+nbr41to@users.noreply.github.com>
2023-01-14 16:33:32 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Discord Event Listener
+
+# TODO
+
+- TypeScript 化
+- 活用時間を DB に保存
+- メンバーの出入り時に Slack の投稿が編集されて,リアルタイムでメンバーの出入りを確認できるようにする
+- メンバーの Commit 時間が DB に累積されるように
---
+++ main.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { WebClient } = require('@slack/web-api');
+const { format } = require('date-fns');
+
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+const DISCORD_LEARNING_CHANNEL_ID = process.env.DISCORD_LEARNING_CHANNEL_ID;
+const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
+const SLACK_LEARNING_CHANNEL_ID = process.env.SLACK_LEARNING_CHANNEL_ID;
+
+const discord = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+});
+
+const slack = new WebClient(SLACK_BOT_TOKEN);
+
+discord.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+/* ボイスチャンネルのイベントを検知 */
+discord.on(Events.VoiceStateUpdate, async (oldState, newState) => {
+  const oldChannel = oldState.channel;
+  const newChannel = newState.channel;
+
+  /* 人数が変化していない場合 */
+  if (oldChannel === newChannel) return;
+  // if (newChannel && newChannel.id !== LEARNING_CHANNEL_ID) return;
+
+  /* 最初の一人を検知 */
+  if (
+    newChannel &&
+    newChannel.id === DISCORD_LEARNING_CHANNEL_ID &&
+    newChannel.members.size === 1
+  ) {
+    try {
+      const nowString = format(new Date(), 'yyyy/MM/dd HH:mm:ss');
+      console.log('oldChannel', newChannel.name);
+      console.log('start');
+      await slack.chat.postMessage({
+        channel: SLACK_LEARNING_CHANNEL_ID,
+        text: `【${nowString}】\nDiscordの自習室で作業を開始したメンバーがいるようです！\n下のURLから参加してみましょう🥳\nhttps://discord.gg/JarxAYjm6C`,
+        unfurl_links: false,
+        unfurl_media: false,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  /* 最後の一人を検知 */
+  if (
+    oldChannel &&
+    oldChannel.id === DISCORD_LEARNING_CHANNEL_ID &&
+    oldChannel.members.size === 0
+  ) {
+    console.log('oldChannel', oldChannel.name);
+    console.log('end');
+  }
+});
+
+discord.login(DISCORD_BOT_TOKEN);
